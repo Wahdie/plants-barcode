c224f6462922abcd14c4abd596fc442add164a92
Wahdie <adieadie266@gmail.com>
2024-07-23 17:45:22 +0700

    Update Image of Raflesia

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
       const urlParams = new URLSearchParams(window.location.search);
       const plantId = urlParams.get('id');
-      const contentDiv = document.getElementById('content');
-      
+      const contentDiv = document.getElementById("content");
       if (plantId) {
-          fetch('plants.json')
-              .then(response => response.json())
-              .then(plants => {
-                  const plant = plants.find(p => p.id === parseInt(plantId));
-                  if (plant) {
-                      const plantHtml = `
+        fetch("plants.json")
+          .then((response) => response.json())
+          .then((plants) => {
+            const plant = plants.find((p) => p.id === parseInt(plantId));
+            if (plant) {
+              const plantHtml = `
                           <div class="container">
                               <img src="${plant.photo}" alt="Foto Tanaman">
                               <h1>${plant.name}</h1>
@@ -28,17 +27,22 @@ document.addEventListener("DOMContentLoaded", function() {
                               </div>
                           </div>
                       `;
-                      contentDiv.innerHTML = plantHtml;
-                  } else {
-                      contentDiv.innerHTML = '<p class="error">Tanaman tidak ditemukan.</p>';
-                  }
-              })
-              .catch(error => {
-                  console.error('Error fetching the JSON:', error);
-                  contentDiv.innerHTML = '<p class="error">Terjadi kesalahan dalam mengambil data.</p>';
-              });
+              contentDiv.innerHTML = plantHtml;
+            } else {
+              alert("Tanaman tidak ditemukan");
+              contentDiv.innerHTML =
+                '<p class="error">Tanaman tidak ditemukan.</p>';
+            }
+          })
+          .catch((error) => {
+            console.error("Error fetching the JSON:", error);
+            contentDiv.innerHTML =
+              '<p class="error">Terjadi kesalahan dalam mengambil data.</p>';
+          });
       } else {
-          contentDiv.innerHTML = '<p class="error">ID tanaman tidak ditemukan.</p>';
+        alert("ID Tanaman tidak ditemukan");
+        contentDiv.innerHTML =
+          '<p class="error">ID tanaman tidak ditemukan.</p>';
       }
   });
   
